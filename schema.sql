dbf44b6662ea9596ea6a364d5a43a6e89b4b13d9
Marylee Pabst <marylee@uconn.edu>
2020-10-13 22:57:25 -0700

    more sql functions

--- schema.sql
+++ schema.sql
@@ -3,28 +3,26 @@ create database employee_db;
 use employee_db;
 
 create table employee (
-    id int PRIMARY KEY auto_increment,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT not null,
-    manager_id INT NOT NULL,
-    foreign key (employee_id) references employee (id)
+id int PRIMARY KEY auto_increment,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+manager_id INT,
+foreign key (role_id) references role (id)
 );
 
 create table role (
-    id int PRIMARY KEY auto_increment,
-    title  VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
+id int PRIMARY KEY auto_increment,
+title  VARCHAR(30) NOT NULL,
+salary DECIMAL(10, 2) NOT NULL,
     -- reference to department role belongs to
-    department_id INT,
-    foreign key (role_id) references role (id)
+department_id INT,
+foreign key (department_id) references department (id)
 );
 
 create table department (
-    id int PRIMARY KEY auto_increment,
-    -- hold name or dept
-    name VARCHAR(30) NOT NULL,
-    foreign key (role_id) references role (id)
+id int PRIMARY KEY auto_increment,
+name varchar(30) not null
 );
 
 select * from employee;
@@ -55,3 +53,16 @@ on subs.manager_id = managers.id
 
 where department.id = 1
 order by salary desc;
+
+Select 
+subs.id,subs.first_name, subs.last_name, role.title as 'job title', department.name as department, role.salary, concat(managers.first_name,' ', managers.last_name) as manager 
+from employee as managers
+right join employee as subs
+on subs.manager_id = managers.id
+left join role
+on subs.role_id = role.id
+left join department
+on department.id = role.department_id
+where managers.id = 3
+order by role.salary desc
+;
